ee09e49b4e265c06f065cb72813a37a809dacf8c
Muhammad Ibrar <abrargulzar2004@gmail.com>
2026-07-31 15:27:34 +0500

    Force HTTPS for CSS assets

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title', 'MediCart - Premium Online Pharmacy')</title>
     <!-- Custom CSS Design System -->
-    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+    <link rel="stylesheet" href="{{ secure_asset('css/style.css') }}">
     <!-- Phosphor Icons for professional iconography -->
     <script src="https://unpkg.com/@phosphor-icons/web"></script>
     @stack('styles')
